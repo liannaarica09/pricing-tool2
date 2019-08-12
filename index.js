62c1e1a1661deca2e05aaa24d2534a371f01d6b4
Maureen Bockrath <maureenbockrath@MacBook-Air.attlocal.net>
2019-08-12 15:38:43 -0700

    package corections

--- index.js
+++ index.js
@@ -4,16 +4,10 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
-const multer = require('multer');
-// const Chatkit = require("@pusher/chatkit-server");
 const cors = require('cors');
-// const fs = require(‘fs’);
 var fs = require('file-system');
 const Schema = mongoose.Schema;
-// const chatkit = new Chatkit.default({
-//     instanceLocator: "v1:us1:9444a659-fe48-4c3c-b739-9445db574fcd",
-//     key: "473a4088-03fe-4fb9-884e-a556b33da82d:+7cQzfwFkdu1zjW7BdYdbDBe3lkwDX0oeJIVwz2fd78="
-// });
+
 
 // Define middleware here
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
@@ -23,34 +17,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 
-// app.post("/users", (req, res) => {
-//     const { username } = req.body;
-//     chatkit
-//         .createUser({
-//             id: username,
-//             name: username
-//         })
-//         .then(() => {
-//             console.log(`User created: ${username}`);
-//             res.sendStatus(201);
-//         })
-//         .catch(err => {
-//             if (err.error === "services/chatkit/user_already_exists") {
-//                 console.log(`User already exists: ${username}`);
-//                 res.sendStatus(200);
-//             } else {
-//                 res.status(err.status).json(err);
-//             }
-//         });
-// });
 app.use(cors());
-// app.use(multer({
-//     dest: `./uploads/`,
-//     rename: function (fieldname, filename) {
-//         return filename;
-//     }
-// }));
-
 
 // Add routes, both API and view
 app.use(routes);
